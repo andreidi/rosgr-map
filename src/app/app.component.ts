3c97e39f52f00d869226bd63710bdce4e7c5c0d5
Andrei Diaconu <diaconuionutandrei@gmail.com>
2024-08-04 11:23:41 +0300

    feat: Defer map component & add loading spinner component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MapComponent } from './map/map.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent],
+  imports: [RouterOutlet, MapComponent, LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
