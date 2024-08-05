6b1f1a8816cacd8f7069fd55488f5af0c61fe38f
Andrei Diaconu <diaconuionutandrei@gmail.com>
2024-08-05 19:14:07 +0300

    perf: Cache location and geojson data in localStorage.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,16 @@ export const ROMANIA_LATLNG = [45.9432, 24.9668];
 export const BASE_GMAPS_URL = 'https://www.google.com/maps/search/?api=1&query=';
 
 export const NEW_LOCATION_FORM_URL = 'https://forms.gle/GFSGPTKv5YgreLTt8';
+
+export const FEEDBACK_FORM_URL = 'https://forms.gle/qXZ8g5rKovr6tLZM8';
+
+export const ASSETS_CONFIG = () => {
+  return {
+    locations: 'assets/locations.json',
+    locationsCacheKey: 'locations-cache',
+    locationsCacheTTL: 24 * 60 * 60 * 1000, // 24 hours
+    geoJSON: 'assets/romania.geojson',
+    geoJSONCacheKey: 'geojson-cache',
+    geoJSONCacheTTL: 72 * 60 * 60 * 1000 // 72 hours
+  }
+};
