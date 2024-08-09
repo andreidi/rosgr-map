9301411b61aa8e4d41762024ba6cae929c775b23
Andrei Diaconu <diaconuionutandrei@gmail.com>
2024-08-09 12:46:34 +0300

    feat: Add loading indicator when fetching location schedule.

--- src/app/location-schedule/location-schedule.component.ts
+++ src/app/location-schedule/location-schedule.component.ts
@@ -1,8 +1,8 @@
-import { Component, effect, inject, input, OnInit } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { Component, effect, inject, input, OnDestroy, OnInit } from '@angular/core';
+import { from, Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { SGRLocationService } from '../../services/supabase/locations.service';
 import { ISGRLocationSchedule } from '../../types/location';
@@ -10,19 +10,36 @@ import { ISGRLocationSchedule } from '../../types/location';
 @Component({
   selector: 'app-location-schedule',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatIconModule, MatProgressBarModule],
   templateUrl: './location-schedule.component.html',
   styleUrl: './location-schedule.component.scss'
 })
-export class LocationScheduleComponent {
+export class LocationScheduleComponent implements OnDestroy {
   locationId = input.required<string>();
-  schedules$!: Observable<ISGRLocationSchedule[]>;
+  schedules: ISGRLocationSchedule[] = [];
+  isLoading = true;
 
   private _locations = inject(SGRLocationService);
+  private _subscription$!: Subscription;
 
   constructor() {
     effect(() => {
-      this.schedules$ = from(this._locations.getLocationSchedule(this.locationId()));
+      this.isLoading = true;
+      this._subscription$ = from(this._locations.getLocationSchedule(this.locationId())).subscribe({
+        next: (value) => {
+          this.schedules = value;
+        },
+        error: (err) => {
+          console.error(err);
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
     });
+  }
+
+  ngOnDestroy(): void {
+    this._subscription$.unsubscribe();
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+    url: 'https://rwnsymmfskpmdwmtpqit.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3bnN5bW1mc2twbWR3bXRwcWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMwNDIwNTUsImV4cCI6MjAzODYxODA1NX0.O9dDTBYrzjxpXL8DOMdxcRP0MfMmPOUN_X0HDMQEfF4'
+  }
 };
