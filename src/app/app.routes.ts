7a03cfb6867de967fbf6bc33f88e65c4b2097ed5
Andrei Diaconu <diaconuionutandrei@gmail.com>
2024-10-29 18:36:49 +0200

    perf: Lazy-load suggest new location route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { MapboardComponent } from './mapboard/mapboard.component';
-import { SuggestLocationComponent } from './suggest-location/suggest-location.component';
 
 export const routes: Routes = [
   { path: '', component: MapboardComponent },
   { path: 'harta', redirectTo: '' },
-  { path: 'sugestie-locatie', component: SuggestLocationComponent },
+  {
+    path: 'sugestie-locatie',
+    loadComponent: () =>
+      import('./suggest-location/suggest-location.component').then(
+        (c) => c.SuggestLocationComponent,
+      ),
+  },
 ];
